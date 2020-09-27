a1d59d44e54f92723072508d686275d4a7a009e4
Abhishek Soni <sonijiabhishek@gmail.com>
2020-09-27 22:02:15 +0530

    Add devise views and customize to save role and name for User while signing up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  ROLES = %w[patient doctor admin]
 
-  enum roles: ROLES
+  has_many :patient_appointments, class_name: 'Appointment', foreign_key: 'patient_id'
+  has_many :doctors, through: :patient_appointments
+
+  has_many :doctor_appointments, class_name: 'Appointment', foreign_key: 'doctor_id'
+  has_many :patients, through: :doctor_appointments
+
+  ROLES = { 'patient' => 'patient', 'doctor' => 'doctor', 'admin' => 'admin' }
+
+  enum role: ROLES
+  validates :role, presence: true
 end
